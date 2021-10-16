546fafd59700f4f4fb67832e662f20bb71dd04d0
Windows10 <abhi.anu003@gmail.com>
2021-10-16 15:18:54 +0530

    final push

--- checker.py
+++ checker.py
@@ -5,16 +5,13 @@ import os
 import requests
 from discord import Webhook, RequestsWebhookAdapter
 from dotenv import load_dotenv
+
 load_dotenv()
 url = os.getenv('url')
 webhook = Webhook.from_url(url, adapter=RequestsWebhookAdapter())
 while True:
-    process = subprocess.run(["ping","iot.ccnet.in","-c","2"], capture_output=True, text=True)
-    standard_out = process.stdout
-    standard_out = standard_out.split("\n")
-    #print(type(standard_out))
-    #print(standard_out)
-    if len(standard_out)>3:
+    r = requests.get("http://iot.ccnet.in:1313/logs")
+    if r.status_code == 200:
         webhook.send(time.ctime()+" API Online")
         print(time.ctime()+" API Online")
         time.sleep(7200)
--- webhook.py
+++
@@ -1,5 +0,0 @@
-import requests
-from discord import Webhook, RequestsWebhookAdapter
-
-webhook = Webhook.from_url("https://discord.com/api/webhooks/898861270005071883/p5YDlpjsh1q-mRz641BKtTOnTd6WMlB10yp13Nok2kKlKqv1XOfasuuMIICPMedDF7lD", adapter=RequestsWebhookAdapter())
-webhook.send("Hello World")
